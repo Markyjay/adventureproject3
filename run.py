79ea80c70dc42621b5e79483c515f9c30c47af16
Mark Young <Markyjay@gmail.com>
2023-10-03 17:20:22 +0000

    Add user permission, name and welcome functions

--- run.py
+++ run.py
@@ -16,4 +16,26 @@ story = SHEET.worksheet('story1')
 
 data = story.get_all_values()
 
-print(data)
+print("Would you like to start your adventure? (yes/no): ")
+
+def get_name():
+    while True:
+        name = input("What is your name? ")
+        if name.isalpha():
+            return name
+        else:
+            print("Invalid name. Please enter only letters for your name.")
+
+def start_adventure():
+    print("\nWelcome to your adventure, {}!".format(player_name))
+
+
+start_choice = input().lower()
+
+if start_choice == "yes":
+    player_name = get_name()
+    start_adventure()
+elif start_choice == "no":
+    print("That's too bad, maybe another time.")
+else:
+    print("Invalid choice. Please enter 'yes' or 'no'.")
