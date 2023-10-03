020fbe3578a95de38f84550e06e3d08bb64e4d67
Mark Young <Markyjay@gmail.com>
2023-10-03 18:17:46 +0000

    input acell method to access worksheet cells

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+import time
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -14,9 +16,9 @@ SHEET = GSPREAD_CLIENT.open('adventure_sheet')
 
 story = SHEET.worksheet('story1')
 
-data = story.get_all_values()
+introduction_cell = story.acell('A1').value
 
-print("Would you like to start your adventure? (yes/no): ")
+print("Would you like to start your adventure? (y/n): ")
 
 def get_name():
     while True:
@@ -28,14 +30,15 @@ def get_name():
 
 def start_adventure():
     print("\nWelcome to your adventure, {}!".format(player_name))
-
+    time.sleep(1)
+    print(introduction_cell)
 
 start_choice = input().lower()
 
-if start_choice == "yes":
+if start_choice == "y":
     player_name = get_name()
     start_adventure()
-elif start_choice == "no":
+elif start_choice == "n":
     print("That's too bad, maybe another time.")
 else:
-    print("Invalid choice. Please enter 'yes' or 'no'.")
+    print("Invalid choice. Please enter 'y' or 'n'.")
