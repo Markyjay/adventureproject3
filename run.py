2b7a4b84a07b82255bf4a2b090747ff2399639da
Mark Young <Markyjay@gmail.com>
2023-10-04 10:42:24 +0000

    create a function to generate a code from the user name

--- run.py
+++ run.py
@@ -17,6 +17,13 @@ SHEET = GSPREAD_CLIENT.open('adventure_sheet')
 story = SHEET.worksheet('story1')
 
 introduction_cell = story.acell('A1').value
+escape_code_cell = story.acell('A2').value
+realization_cell = story.acell('A3').value
+goback_cell = story.acell('B1').value
+goforward_cell = story.acell('C1').value
+goleft_cell = story.acell('D1').value
+goright_cell = story.acell('E1').value
+
 
 print("Would you like to start your adventure? (y/n): ")
 
@@ -28,10 +35,42 @@ def get_name():
         else:
             print("Invalid name. Please enter only letters for your name.")
 
+def generate_escape_code(name):
+    name = name.lower()
+    last_letter = name[-1]
+    num_letters = len(name)
+    alpha_name = ''.join(sorted(name))
+    second_letter = alpha_name[1]
+    escape_code = f"{last_letter}{num_letters}{second_letter}"
+    return escape_code
+
 def start_adventure():
-    print("\nWelcome to your adventure, {}!".format(player_name))
+    print("\nWelcome to your adventure {}!".format(player_name))
+    time.sleep(1)
+    print("Make your choices by typing the value diplayed in brackets.")
     time.sleep(1)
     print(introduction_cell)
+    time.sleep(2)
+    print(escape_code_cell)
+    time.sleep(2)
+
+    escape_code = generate_escape_code(player_name)
+    
+    code_attempts = 3
+    while code_attempts > 0:
+        user_code = input("Enter the code to escape: ")
+        if user_code == escape_code:
+            print("Congratulations! You've successfully escaped the room.")
+            break
+        else:
+            code_attempts -= 1
+            if code_attempts > 0:
+                print(f"Incorrect code. You have {code_attempts} attempts left.")
+            else:
+                print("Sorry, you've run out of attempts. The room remains locked.")
+                break
+
+    print(realization_cell)   
 
 start_choice = input().lower()
 
